8495d7cb2c147e7eb94c0e9399d18fc31a9c1de8
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-11 10:30:01 -0700

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
             "403": 0,
             "404": 0,
             "405": 0,
-            "500": 0,
+            "500": 0
         }
         file_size = 0
         count_lines = 0
@@ -24,18 +24,18 @@ if __name__ == '__main__':
         print("File size: {:d}".format(file_size))
         for key, value in sorted(status_code.items()):
             if value:
-                print("{}:{:d}".format(key, value))
+                print("{}: {:d}".format(key, value))
 
         try:
             for line in stdin:
                 if count_lines % 10 == 0 and count_lines != 0:
                     parse_log(status_code, file_size)
-                count_lines += 1
                 data = line.split(" ")
-                arg = data[-2]
-                if arg in status_code:
-                    status_code[arg] += 1
-                file_size += int(data[-1])
+                if len(data):
+                    arg = data[-2]
+                    if arg in status_code:
+                        status_code[arg] += 1
+                    file_size += int(data[-1])
             parse_log(status_code, file_size)
         except KeyboardInterrupt as error:
             parse_log(status_code, file_size)
