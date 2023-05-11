13fde842f3001374d4048630c3c7e16fbb5a4589
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-11 10:34:59 -0700

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,9 +22,9 @@ if __name__ == '__main__':
         count_lines = 0
 
         print("File size: {:d}".format(file_size))
-        for key, value in sorted(status_code.items()):
-            if value:
-                print("{}: {:d}".format(key, value))
+        for key in sorted(status_code.keys()):
+            if status_code[key]:
+                print("{}: {:d}".format(key, status_code[key]))
 
         try:
             for line in stdin:
