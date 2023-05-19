2bca7837950b702055036239c6b790fbf4b81dc5
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-19 00:11:18 -0700

    utf8-validation-project: bugs are fixed again

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,22 +6,29 @@
 def validUTF8(data):
     """ Define a method to determine a valid UTF-8 encoding
     """
+    # 1000 0000
+    byte1 = 1 << 7
+    # 0100 0000
+    byte2 = 1 << 6
+    numOfBytes = 0
     if type(data) is not list:
         return False
     if len(data) == 0 or not data:
         return True
-    if len(data) == 1:
-        if data[0] >= 32 and data[0] <= 126:
-            return True
-        else:
-            return False
     for n in data:
-        if type(n) is not int:
-            return False
-        if n > 31 and n < 127:
-            continue
-        elif n == 0:
-            continue
+        byte = 1 << 7
+        if numOfBytes == 0:
+            while (byte & n):
+                numOfBytes += 1
+                byte = byte >> 1
+            if numOfBytes == 0:
+                continue
+            if numOfBytes == 1 or numOfBytes > 4:
+                return False
         else:
-            return False
+            if not (n & byte1 and not (n & byte2)):
+                return False
+        numOfBytes -= 1
+    if numOfBytes:
+        return False
     return True
