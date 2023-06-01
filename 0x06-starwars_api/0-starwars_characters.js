02db172ac429c78f2a09d742c47ed99711ced1ea
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-01 13:55:31 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,27 +2,29 @@
 // a script that prints all characters of a Star Wars movie
 const request = require('request');
 
-const url = `https://swapi-api.alx-tools.com/api/${process.argv[2]}/`;
+if (process.argv.length === 3) {
+  const url = `https://swapi-api.alx-tools.com/api/${process.argv[2]}/`;
 
-// Making API requests
-request(url, async (error, res, body) => {
-  if (error) return console.log(error);
+  // Making API requests
+  request(url, async (error, res, body) => {
+    if (error) return console.log(error);
 
-  // each character url
-  const urlList = JSON.parse(body).characters;
+    // each character url
+    const urlList = JSON.parse(body).characters;
 
-  // Get all characters
-  for (let chars of urlList) {
-    const answer = () => {
-      return new Promise((resolve, reject) => {
-      request(chars, async (error, res, body) => {
-        if (error) {
-          console.log(error);
-        }
-        resolve(JSON.parse(body).name);
+    // Get all characters
+    for (let chars of urlList) {
+      const answer = () => {
+        return new Promise((resolve, reject) => {
+        request(chars, async (error, res, body) => {
+          if (error) {
+            console.log(error);
+          }
+          resolve(JSON.parse(body).name);
+          });
         });
-      });
+      }
+      console.log(await answer());
     }
-    console.log(await answer());
-  }
-});
+  });
+}
