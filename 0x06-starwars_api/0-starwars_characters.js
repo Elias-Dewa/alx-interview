45273fc8ffeb403a9cc7d2c14f2e4fa3e1425fe0
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-01 13:25:07 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,12 +2,11 @@
 // a script that prints all characters of a Star Wars movie
 const request = require('request');
 
-const movieId = process.env.splice(2)
-const url = `https://swapi-api.alx-tools.com/api/${movieId[0]}/`;
+const url = `https://swapi-api.alx-tools.com/api/${process.argv[2]}/`;
 
 // Making API requests
 request(url, async (error, res, body) => {
-  if (error) throw new Error(error);
+  if (error) return console.log(error);
 
   // each character url
   const urlList = JSON.parse(body).characters;
@@ -16,7 +15,7 @@ request(url, async (error, res, body) => {
   for (let chars of urlList) {
     await new Promise((resolve, reject) => {
       request(chars, async (error, res, body) => {
-        if (error) throw new Error(error);
+        if (error) return console.log(error);
         console.log(JSON.parse(body).name);
         resolve();
       });
