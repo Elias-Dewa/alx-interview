8a871b73866d68ad803fb62819611c1c7cb1257c
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-03 13:46:17 -0700

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,39 +4,41 @@
 
 import sys
 
+if __name__ == '__main__':
 
-status_code = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-file_size = 0
-total_file_sizes = 0
-count_lines = 0
+    file_size = 0
+    total_file_sizes = 0
+    count_lines = 0
 
-try:
-    for line in sys.stdin:
-        data = line.split()
-        if len(data) >= 2:
-            arg = data[-2]
-            file_size = int(data[-1])
-            if arg in status_code.keys():
-                status_code[arg] += 1
-            total_file_sizes += file_size
-            count_lines += 1
-        if count_lines % 10 == 0:
-            print("File size: {}".format(total_file_sizes))
-            for key, value in sorted(status_code.items()):
-                if value:
-                    print("{}: {}".format(key, value))
-            count_lines = 0
-except KeyboardInterrupt:
-    print("File size: {}".format(total_file_sizes))
-    for key, value in sorted(status_code.items()):
-        if value:
-            print("{}: {}".format(key, value))
+    status_code = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+
+    try:
+        for line in sys.stdin:
+            data = line.split()
+            if len(data) >= 2:
+                arg = data[-2]
+                file_size = int(data[-1])
+                if arg in status_code.keys():
+                    status_code[arg] += 1
+                total_file_sizes += file_size
+                count_lines += 1
+            if count_lines % 10 == 0:
+                print("File size: {}".format(total_file_sizes))
+                for key, value in sorted(status_code.items()):
+                    if value:
+                        print("{}: {}".format(key, value))
+                count_lines = 0
+    except KeyboardInterrupt:
+        print("File size: {}".format(total_file_sizes))
+        for key, value in sorted(status_code.items()):
+            if value:
+                print("{}: {}".format(key, value))
