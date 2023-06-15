214eb2789f43a0d213b4ce7d03a753f7b24d8338
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-15 02:42:26 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,14 +6,17 @@ def makeChange(coins, total):
     """Make a change to a given amount of coins"""
     if total < 1:
         return 0
+
     coinsChange = 0
     coins.sort()
     coins.reverse()
+
     for i in coins:
         change = int(total / i)
         total -= change * i
         coinsChange += change
         if total == 0:
             return coinsChange
+
     if total != 0:
         return -1
